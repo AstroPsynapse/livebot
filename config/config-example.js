434930fced1470d1d9cf91b0d3206ea1872c7ee2
AstroPsynapse <bwDozer235@gmail.com>
2018-02-01 08:54:15 -0700

    Bring config-example.js up to date

--- config/config-example.js
+++ config/config-example.js
@@ -3,21 +3,17 @@
 // to config.js!
 //
 // The client key for the bot to connect and authorize
-let clientKey = 'YOUR CLIENT KEY HERE';
-// The roles for permissions
-let permRoles = ['Admin', 'Moderator'];
+let clientKey = '<keygoeshere>';
 // Name of the server to operate on
-let guildName = "My Server Name";
+let guildName = "Server Name";
 // The prefix symbol (or word) to use before commands.
-let cmdPrefix = "!";
+let prefix = ".";
 // Name of the channel that the bot will announce to for live streams.
-let announceChannel = "streams";
-// Name of the channel the bot operates on. Set it to "" for all channels.
-let activeChannel = "";
+let streamChannel = "streams";
 // For development purposes. Includes extra verbose logging.
 let debug = false;
 // Don't touch unless you know what you're doing!
-let configFile = './config/streamers.json';
+let streamerFile = './config/streamers.json';
 // Don't touch unless you know what you're doing!
-let refreshRate = 25000;
-module.exports = {clientKey, guildName, cmdPrefix, announceChannel, activeChannel, permRoles, debug, configFile, refreshRate};
+let refreshRate = 30000;
+module.exports = {clientKey, guildName, prefix, streamChannel, debug, streamerFile, refreshRate};
