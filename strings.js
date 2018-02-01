70fef9a97fc0be387a334f8d50600c412f9367af
AstroPsynapse <bwDozer235@gmail.com>
2018-02-01 08:54:48 -0700

    Add additional error strings

--- strings.js
+++ strings.js
@@ -1,5 +1,9 @@
 module.exports = {
   readError:`***An unexpected error has occurred while reading. Please report this to the bot creator!***`,
   writeError: `***An unexpected error has occurred while writing. Please report this to the bot creator!***`,
-  commandSyntaxError: `Error - Invalid command syntax.`
+  commandSyntaxError: `Error - Invalid command syntax.`,
+  schedulerFatalError: `FATAL ERROR: The scheduler could not access the streamer storage file.`,
+  guildNotFoundError: `FATAL ERROR: The server specified in config.js could not be found. Please double check the name.`,
+  channelNotFoundError: `FATAL ERROR: The channel specified in config.js could not be found. Please double check the name.`,
+  noStreamersFoundError: `No streamers were found in the streamer storage file.`
 }
