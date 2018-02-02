84e417ea1e0ff54dc039939c1085b6010769b009
AstroPsynapse <bwDozer235@gmail.com>
2018-02-01 21:50:11 -0700

    Start work on different livestream api handling

--- scheduler.js
+++ scheduler.js
@@ -24,26 +24,27 @@ module.exports = {
         var x = 0;
         for (var i = 0; i < obj.length; i++) {
             if (obj[i].hasOwnProperty("name"))
-            request(`https://api.picarto.tv/v1/channel/name/${obj[i].name}`, function(err, resp, body) {
-              if (err) console.log(`Scheduler Error - Could not request from Picarto API for user ${obj[this.i].name}`);
-              else {
-                var data = JSON.parse(body);
-                if (!obj[this.i].hasOwnProperty("live")) {
-                  obj[this.i].live = false;
-                }
-                if (obj[this.i].live != data.online) {
-                  changed = true;
-                  obj[this.i].live = data.online;
-                  if (obj[this.i].live == true)
-                    channel.send(`${data.name} just went live on Picarto.tv! Watch live here: https://picarto.tv/${data.name}`);
-                  else
-                    channel.send(`${data.name} stopped streaming on Picarto.tv`);
-                }
-                x++;
-                if (x == obj.length && changed)
-                  save(obj);
-              }
-            }.bind({i: i}));
+              if (obj[i].hasOwnProperty("api"))
+                request(`https://api.picarto.tv/v1/channel/name/${obj[i].name}`, function(err, resp, body) {
+                  if (err) console.log(`Scheduler Error - Could not request from Picarto API for user ${obj[this.i].name}`);
+                  else {
+                    var data = JSON.parse(body);
+                    if (!obj[this.i].hasOwnProperty("live")) {
+                      obj[this.i].live = false;
+                    }
+                    if (obj[this.i].live != data.online) {
+                      changed = true;
+                      obj[this.i].live = data.online;
+                      if (obj[this.i].live == true)
+                        channel.send(`${data.name} just went live on Picarto.tv! Watch live here: https://picarto.tv/${data.name}`);
+                      else
+                        channel.send(`${data.name} stopped streaming on Picarto.tv`);
+                    }
+                    x++;
+                    if (x == obj.length && changed)
+                      save(obj);
+                  }
+                }.bind({i: i}));
         }
       });
     }, config.refreshRate);
